c8cbf3917f982002183fad7bf553ab51763038f3
NaokiDoki <103157685+NaokiDoki@users.noreply.github.com>
2022-04-08 07:25:01 +0545

    Add files via upload

---
+++ operations.h
@@ -0,0 +1,28 @@
+
+
+void add(float a, float b){
+	float sum = a + b;
+	printf("The Sum is %f\n",sum);
+}
+
+void sub(float a, float b){
+	float sub;
+	sub = a - b;
+	printf("The difference is %f\n",sub);
+}
+
+void mul(float a, float b){
+	float mul;
+	mul = a * b;
+	printf("The multiplication is %f\n",mul);
+}
+
+void div(float a, float b){
+	float div;
+	div = a / b;
+	printf("The Division of is %f\n",div);
+}
+
+
+
+
---
+++ operations123.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"operations.h"
+
+int main(){
+	float a = 10.0, b = 5.0;
+	add(a,b);
+	sub(a,b);
+	mul(a,b);
+	div(a,b);
+	return 0;
+	
+}
